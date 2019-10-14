71273b83d4b315f2c4efbafa632c272a7a4b5c5f
Vicente Manuel Munoz Milchorena <vicosurge06@gmail.com>
2019-10-13 23:47:17 -0700

    Still working on logic for program to work, not getting it right

--- baktun.py
+++ baktun.py
@@ -1,19 +1,37 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="This script can upload documents to our blogspot blog, refer to the flags below for more information")
+parser.add_argument("-d","--debug", help="Debug application, set to 1 for debuging, default 0", 
+    default=0, type=int, dest="debug")
+flags = parser.parse_args()
+
 def cuenta_larga():  
-    print """ 
+    print("""
     Este script calcula la cuenta larga Maya y la convierte al Gregoriano.  
     Es un aproximado que da unicamente el anyo, no da mes ni dia. 
     Dentro del codigo se contiene el comentario de como funciona el codigo. 
-    """  
-    baktun = input("Baktun: ")  
-    katun = input("Katun: ")  
-    tun = input("Tun: ")  
-    uinal = input("Uinal: ")  
-    kin = input("Kin: ")  
-    baktun = baktun*144000  
-    katun = katun*7200  
-    tun = tun*360  
-    uinal = uinal*20  
-    kin = kin  
+    """)
+    baktun = int(input("Baktun: "))
+    baktun = baktun*144000
+    if flags.debug == 1:
+        print(baktun)
+        input("Wait...")
+    katun = int(input("Katun: "))
+    katun = katun*7200
+    if flags.debug == 1:
+        print(katun)
+        input("Wait...")
+    tun = int(input("Tun: "))
+    tun = tun*360
+    if flags.debug == 1:
+        print(tun)
+        input("Wait...")
+    uinal = int(input("Uinal: "))
+    uinal = uinal*20
+    if flags.debug == 1:
+        print(uinal)
+        input("Wait...")
+    kin = int(input("Kin: "))
     # Las cantidades manejadas son dias, un baktun es 144000 dias, un katun es 7200,  
     # un tun 360, un uinal es 20 y los kin son unidades individuales. Todas estas se  
     # suman bajo la variable de gTotal para despues ser dividas entre 365, que es la  
@@ -23,17 +41,24 @@ def cuenta_larga():
     # negativo, es decir marca -X este tendra el signo de menos removido y en lugar  
     # de marcar d.e.C. marcara a.e.C. como debe corresponder por ser previo a la   
     # epoca que nosotros demarcamos como la era Comun.  
-    gTotal = baktun+katun+tun+uinal+kin  
-    print gTotal  
-    gregoriano = gTotal / 365  
-    print gregoriano  
-    maya = gregoriano - 3115  
-    if "-" in str(maya):  
-        a = str(maya)  
-        final = a.replace("-","")  
-        print final, "a.C."  
-    else:  
-        print maya, "d.C."  
-        stop = raw_input("Presiona cualquier tecla para terminar...")  
-  
+    gTotal = baktun+katun+tun+uinal+kin
+    print("Total Maya:",gTotal)
+    years = 0
+    total_days = int(gTotal)
+    print(total_days)
+    while total_days >= 0:
+        for i in range(3):
+            total_days -= 365
+            years += 1
+        total_days -= 366
+        years += 1
+    print(years,years-3115,total_days)
+#    if "-" in str(maya):
+#        a = str(maya)
+#        final = a.replace("-","")
+#        print(final, "a.C.")
+#    else:
+#        print(maya, "d.C.")
+#        input("Presiona cualquier tecla para terminar...")
+
 cuenta_larga()
